81e60eea1c3395829c3a03fcbb070a3d95f221d7
Zane <subitbravepoudel7@gmail.com>
2026-06-01 20:34:12 +0545

    Initial commit- The Road to React By Robin Wieruch

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  function handleSearch(e) {
+    setSearchTerm(e.target.value);
+  }
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = ({ searchTerm, onSearch }) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={searchTerm} onChange={onSearch} />
+    </div>
+  );
+};
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>{" "}
+    <span>{item.author}</span> <span>{item.num_comments}</span>{" "}
+    <span>{item.points}</span>
+  </li>
+);
+
+export default App;
